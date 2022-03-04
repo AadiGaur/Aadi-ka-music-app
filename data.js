bb5895a2fa0d9e0dfd23caf5c7190e303a38a6c6
AadiGaur <93583742+AadiGaur@users.noreply.github.com>
2022-03-04 16:28:59 +0530

    Initial Commit

---
+++ data.js
@@ -0,0 +1,74 @@
+let songs = [
+    {
+        name: 'Tum Ho',
+        path: 'musics/Song 1.mp3',
+        artist: 'Mohit Chauhan, Suzanne D Mello',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'Kun Faya Kun',
+        path: 'musics/Song 2.mp3',
+        artist: 'A.R Rahman',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'Can We Kiss Forever',
+        path: 'musics/Song 3.mp3',
+        artist: 'Kina',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'Love Nwantiti',
+        path: 'musics/Song 4.mp3',
+        artist: 'CKay',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'Mere Wala Sardar',
+        path: 'musics/Song 5.mp3',
+        artist: 'Jugraj Sandhu',
+        cover: 'images/cover 5.png'
+    },
+    {
+        name: 'Mere Sapno Ki Rani',
+        path: 'musics/Song 6.mp3',
+        artist: 'Kishore Kumar',
+        cover: 'images/cover 6.png'
+    },
+    {
+        name: 'Illegal Weapon 2.0',
+        path: 'musics/Song 8.mp3',
+        artist: 'Garry Sandhu',
+        cover: 'images/cover 8.png'
+    },
+    {
+        name: 'Gori Teri Aankhen',
+        path: 'musics/Song 9.mp3',
+        artist: 'Lucky Ali, Kavita Krishnamurthy',
+        cover: 'images/cover 9.png'
+    },
+    {
+        name: 'Ikko - Mikke',
+        path: 'musics/Song 10.mp3',
+        artist: 'Satinder Sartaaj',
+        cover: 'images/cover 10.png'
+    },
+    {
+        name: 'Peaches',
+        path: 'musics/Song 11.mp3',
+        artist: 'Justin Bieber',
+        cover: 'images/cover 11.png'
+    },
+    {
+        name: 'Stay',
+        path: 'musics/Song 12.mp3',
+        artist: 'Justin Bieber',
+        cover: 'images/cover 12.png'
+    },
+    {
+        name: 'Satisfya',
+        path: 'musics/Song 13.mp3',
+        artist: 'Imran Khan',
+        cover: 'images/cover 13.png'
+    },
+]
